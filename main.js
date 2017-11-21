cb1ac125cc906f2c5efb15897c09c495221ffbc6
mcisaachm <33849285+mcisaachm@users.noreply.github.com>
2017-11-20 20:19:08 -0400

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-
-$(document).ready(function() {
-
-        $("#hamburger, #close").click(function() {
-          $("#hamburger, #close").toggle();
-          $("nav ul").toggleClass("open-nav");
-
-        });
-      });
